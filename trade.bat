1e88ed80bac38cdc2c5097fd986e78dce23dc0ae
gauravjaindhuri <80357883+gauravjaindhuri@users.noreply.github.com>
2021-03-10 13:00:28 +0530

    Add files via upload
    Use this project through this bat file

---
+++ trade.bat
@@ -0,0 +1,14 @@
+@echo off
+
+
+echo This Demo is for Trade Store by Gaurav Jain  . Ther are 3 operations you can perform :
+
+echo show
+echo add T4 2 CP-1 B2 11/04/2021
+echo show T2
+
+set /p operation=Enter Operation?:
+
+java -jar gjtradestore-0.0.1-SNAPSHOT.jar %operation%
+
+pause
