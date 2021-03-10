1269b81df9caa4b33b011ccb9620bb6db6c868b6
gauravjaindhuri <80357883+gauravjaindhuri@users.noreply.github.com>
2021-03-10 13:25:36 +0530

    Update MainApp.java

--- src/main/java/com/demo/MainApp.java
+++ src/main/java/com/demo/MainApp.java
@@ -19,102 +19,92 @@ import com.demo.exception.LowerVersionException;
 public class MainApp {
 
 	private static final Logger logger = LoggerFactory.getLogger(MainApp.class);
-	
 
 	public static void main(String[] args) {
-		
-		try
-		{
-		
-		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		
-	
-		
-		
-		
 
-		if (args != null && args.length > 0) {
+		try {
 
-			ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-beans.xml");
+			DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-			MarketPlace main = (MarketPlace) applicationContext.getBean("marketPlace");
+			if (args != null && args.length > 0) { 
 
-			if (args.length == 1 && args[0].equals("start")) {
+				ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-beans.xml");
 
-				main.start();
+				MarketPlace main = (MarketPlace) applicationContext.getBean("marketPlace");
 
-			}
-			if (args.length == 1 && args[0].equals("stop")) {}
+				if (args.length == 1 && args[0].equals("start")) {
 
-			if (args.length == 1 && args[0].equals("show")) {
+					main.start();
 
-				Set<TradeEntity> tradeList = main.showAll();
-				
-				
-
-				for (TradeEntity rr : tradeList) {
-					System.out.println(
-							rr.getTradeId() + "|" + rr.getVersion() + "|" + rr.getCounterParty() + "|" + rr.getBookId()
-									+ "|" + dateFormat.format(rr.getMaturityDate()) + "|" + dateFormat.format(rr.getCreatedDate()) + "|" + rr.isExpired());
+				}
+				if (args.length == 1 && args[0].equals("stop")) {
 				}
 
-			}
+				if (args.length == 1 && args[0].equals("show")) {
 
-			if (args.length == 2 && args[0].equals("show")) {
+					Set<TradeEntity> tradeList = main.showAll();
 
-				main.showTrade(args[1]);
-			}
+					for (TradeEntity trade : tradeList) {
+						System.out.println(trade.getTradeId() + "|" + trade.getVersion() + "|" + trade.getCounterParty() + "|"
+								+ trade.getBookId() + "|" + dateFormat.format(trade.getMaturityDate()) + "|"
+								+ dateFormat.format(trade.getCreatedDate()) + "|" + trade.isExpired());
+					}
 
-			if (args.length > 2 && args[0].equals("add")) {
-
-				try {
-
-					String tradeId = args[1];
-
-					int version = Integer.parseInt(args[2]);
-
-					String counterParty = args[3];
-					String bookId = args[4];
-					boolean expire = false;
-
-					Date maturity = new SimpleDateFormat("dd/MM/yyyy").parse(args[5]);
-
-					Date createdDate = new Date();
-
-					TradeEntity tentity = new TradeEntity(tradeId, version, counterParty, bookId, maturity, createdDate,
-							expire);
-
-					main.addTrade(tentity);
-
-				} catch (ArrayIndexOutOfBoundsException e) {
-					logger.error(Constants.INVALID_INPUT + e);
-					System.out.println(Constants.INVALID_INPUT + e);
-				} catch (NumberFormatException e1) {
-					logger.error(Constants.INVALID_INPUT + e1);
-					System.out.println(Constants.INVALID_INPUT + e1);
-				} catch (ParseException e) {
-					logger.error(Constants.INVALID_INPUT + e);
-					System.out.println(Constants.INVALID_INPUT + e);
-				} catch (LowerVersionException e) {
-					logger.error(Constants.OLDER_VERSION + e);
-					System.out.println(Constants.OLDER_VERSION + e);
 				}
 
+				if (args.length == 2 && args[0].equals("show")) {
+
+					main.showTrade(args[1]);
+				}
+
+				if (args.length == 6 && args[0].equals("add")) {
+
+					try {
+
+						String tradeId = args[1];
+
+						int version = Integer.parseInt(args[2]);
+
+						String counterParty = args[3];
+						String bookId = args[4];
+						boolean expire = false;
+
+						Date maturity = new SimpleDateFormat("dd/MM/yyyy").parse(args[5]);
+
+						Date createdDate = new Date();
+
+						TradeEntity tentity = new TradeEntity(tradeId, version, counterParty, bookId, maturity,
+								createdDate, expire);
+
+						main.addTrade(tentity);
+
+					} catch (ArrayIndexOutOfBoundsException e) {
+						logger.error(Constants.INVALID_INPUT + e);
+						System.out.println(Constants.INVALID_INPUT + e);
+					} catch (NumberFormatException e1) {
+						logger.error(Constants.INVALID_INPUT + e1);
+						System.out.println(Constants.INVALID_INPUT + e1);
+					} catch (ParseException e) {
+						logger.error(Constants.INVALID_INPUT + e);
+						System.out.println(Constants.INVALID_INPUT + e);
+					} catch (LowerVersionException e) {
+						logger.error(Constants.OLDER_VERSION + e);
+						System.out.println(Constants.OLDER_VERSION + e);
+					}
+
+				}
+
+				((ClassPathXmlApplicationContext) applicationContext).close();
+
 			}
 
-			((ClassPathXmlApplicationContext) applicationContext).close();
+			else {
+				logger.error(Constants.INVALID_INPUT);
+				System.out.println(Constants.INVALID_INPUT);
+			}
 
-		}
+		} catch (Exception e) {
 
-		else {
-			logger.error(Constants.INVALID_INPUT);
-			System.out.println(Constants.INVALID_INPUT);
-		}
-		
-		}
-		catch(Exception e)
-		{
-			
 		}
 	}
 
